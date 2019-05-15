f66b6e9b5a86a2e2bf08b476e9d16185a8599aa4
david <david@davidberry.io>
2019-05-15 10:05:07 -0700

    start of scaffold, event handlers for buttons

---
+++ app/app.js
@@ -0,0 +1,26 @@
+/*
+
+ ### Basic Reqs
+- [ ] Where to store data? (localstorage)
+- [ ] How to caputure data? (web form)
+- [ ] How to modify data? (update action, delete action)
+- [ ] How to view data? (style?)
+- [ ] UI/UX considerations (how are we going to use this)
+
+*/
+
+//localStorage interaction function
+
+//event handlers for the buttons and ... possibly the inputboxes
+  //preventdefault on button clicks
+$(document).ready(function() {
+  $('#createButton').click(function(event) {
+    event.preventDefault();
+    console.log('createButton was clicked');
+  });
+
+  $('#updateButton').click(function(event) {
+    event.preventDefault();
+    console.log('updateButton was clicked');
+  });
+});
