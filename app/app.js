01146e618f1c46f493f962dc7f72fc359a66f8ee
david <david@davidberry.io>
2019-05-15 10:30:31 -0700

    started to interact with localStorage

--- app/app.js
+++ app/app.js
@@ -10,6 +10,11 @@
 */
 
 //localStorage interaction function
+//get item
+var getItem = function(key) {
+  return window.localStorage.getItem(key);
+}
+
 //create
 var createItem = function(key, value) {
   return window.localStorage.setItem(key, value);
@@ -30,6 +35,11 @@ var clearEverything = function() {
   return window.localStorage.clear();
 }
 
+var keyExists = function(key) {
+  var currentValue = getItem(key);
+  return currentValue !== null;
+}
+
 
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
@@ -37,11 +47,25 @@ var clearEverything = function() {
 $(document).ready(function() {
   $('#createButton').click(function(event) {
     event.preventDefault();
-    console.log('createButton was clicked');
+
+    var currentKey = $("#keyInput").val();
+    var currentValue = $("#valueInput").val();
+    if (keyExists(currentKey)) {
+      //current key exists, do something error-handle-y
+    } else {
+      createItem(currentKey, currentValue);
+    }
   });
 
   $('#updateButton').click(function(event) {
     event.preventDefault();
-    console.log('updateButton was clicked');
+
+    var currentKey = $("#keyInput").val();
+    var currentValue = $("#valueInput").val();
+    if (keyExists(currentKey)) {
+      updateItem(currentKey, currentValue);
+    } else {
+      //current key doesnt exist, do stuff
+    }
   });
 });
