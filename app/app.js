c0645e8e1d8f0d138ebc1bdb26d03ebb6e05986b
zonagilreath <bryson.gilreath@gmail.com>
2019-05-16 13:28:23 -0500

    basic/advanced search form alternation working

--- app/app.js
+++ app/app.js
@@ -1,22 +1,83 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.querySelector('form').addEventListener('submit', function(event){
-		event.preventDefault();
-		console.log(event.target);
-	  let formData = new FormData(event.target);
-	  let titleSearch = formData.get('book_title');
-	  let books = JSON.parse(window.localStorage.getItem('books'));
-	  for (let key in books) {
-	  	if (key.includes(titleSearch.toLowerCase())){
-	  		console.log(key);
-	  		console.log(books[key]);
-	  	}
-	  }
-	});
+	getSearchForm('basic');
 });
 
-function getFormData(){
-	let formData = new FormData(document.querySelector('form'));
-  let dataObj = {};
-	formData.forEach((value, key) => {dataObj[key] = value});
-  console.log(dataObj);
+
+function formHandler(event, searchFunction){
+	event.preventDefault();
+	let formData = new FormData(event.target);
+	searchFunction(formData);
 }
+
+function getSearchForm(basicOrAdvanced, titleSearchString){
+	// console.log('getSearch called with: ', basicOrAdvanced, titleSearchString);
+	let specifics = {
+		basic: [basicSearchForm, basicSearch],
+		advanced: [advancedSearchForm, advancedSearch],
+	}
+
+	document.querySelector('#form_container').innerHTML = specifics[basicOrAdvanced][0];
+
+	let form = document.querySelector('form');
+	if (titleSearchString) {
+		form.elements['book_title'].value = titleSearchString;
+	}
+
+	form.addEventListener('submit', function(event){
+		formHandler(event, specifics[basicOrAdvanced][1]);
+	});
+
+	let otherForm = (basicOrAdvanced === 'basic')? 'advanced' : 'basic';
+	let changeFormButton = document.querySelector('#change_form');
+	changeFormButton.setAttribute('jsOtherForm', otherForm);
+	changeFormButton.addEventListener('click', function(event){
+		let titleSearchString = document.querySelector('form').elements['book_title'].value;
+		let basicOrAdvenced = event.target.getAttribute('jsOtherForm');
+		getSearchForm(basicOrAdvenced, titleSearchString);
+	})
+}
+
+// function getBasicSearchForm(titleSearchString){
+// 	let formContainer = document.querySelector('.form_container');
+// 	formContainer.innerHTML = basicSearchForm;
+// 	let form = document.querySelector('form');
+// 	if (titleSearchString) {
+// 		form.elements['book_title'].value = titleSearchString;
+// 	}
+// 	form.addEventListener('submit', function(event){
+// 		formHandler(event, basicSearch);
+// 	});
+// }
+
+// function getAdvancedSearchForm(titleSearchString){
+// 	let formContainer = document.querySelector('.form_container');
+// 	formContainer.innerHTML = advancedSearchForm;
+// 	let form = document.querySelector('form');
+// 	if (titleSearchString) {
+// 		form.elements['book_title'].value = titleSearchString;
+// 	}
+// 	form.addEventListener('submit', function(event){
+// 		formHandler(event, advancedSearch);
+// 	});
+// }
+
+function basicSearch(formData){
+	let titleSearch = formData.get('book_title');
+  let books = JSON.parse(window.localStorage.getItem('books'));
+  for (let key in books) {
+  	if (key.includes(titleSearch.toLowerCase())){
+  		console.log(key);
+  		console.log(books[key]);
+  	}
+  }
+}
+
+function advancedSearch(formData){
+
+}
+// function getFormData(){
+// 	let formData = new FormData(document.querySelector('form'));
+//   let dataObj = {};
+// 	formData.forEach((value, key) => {dataObj[key] = value});
+//   console.log(dataObj);
+// }
